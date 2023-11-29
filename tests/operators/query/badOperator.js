2e50f05d29ce0adabeac20f17663f03b6dcb79f7
Oliver Foster <oliver.foster@kineo.com>
2023-11-29 23:12:16 +0000

    Update

--- tests/operators/query/badOperator.js
+++ tests/operators/query/badOperator.js
@@ -1,7 +1,19 @@
 import { queryToPredicate } from 'json-mongo-query'
 
-test('bad operator', () => {
+test('bad operator $missing', () => {
   expect(() => {
     queryToPredicate({ $missing: 1 })
   }).toThrow('Operator $missing is not supported')
+})
+
+test('bad operator $', () => {
+  expect(() => {
+    queryToPredicate({ $: 1 })
+  }).toThrow('Operator $ is not supported')
+})
+
+test('bad operator $[element]', () => {
+  expect(() => {
+    queryToPredicate({ '$[element]': 1 })
+  }).toThrow('Operator $[element] is not supported')
 })
--- todo.md
+++ todo.md
@@ -1,4 +1,3 @@
 ### Other
 1. lexicographic vs numeric order is not currently supported https://www.mongodb.com/docs/manual/reference/operator/update/#behavior
-2. implement $ and $[identifier] or throw error
 
